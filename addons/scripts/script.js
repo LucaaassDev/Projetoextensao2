43d5fe7fca8af6a0f0610992d33eeb50b2a85ac5
Alef Teles <alef.teles@americaenergia.com.br>
2025-06-01 22:41:43 -0300

    Implementando novo layout e funcionalidades para o site do Instituto Tia Lia - Inclusão de liders interativos via CSS e JavaScript - Adição de novas seções de conteúdo - Melhorias na responsividade - Adicionadas novas imagens e estilos para aprimorar a apresentação visual - Refatoração do código em partes de quebra de lógica - Limpeza de classes e id's redundantes - Reestruturação completa do html - Adição de animações de botões - Adição de vetores - Adicionadas cores corretas do protótipo - Adicionada fonte correta do protótipo - Adição de ícones de redes sociais - Remoção de partes inviáveis do código - Correção de links externos - Correção de Menu - Adição de links de navegação internos - Correção de uso de tag's semânticas

---
+++ addons/scripts/script.js
@@ -0,0 +1,73 @@
+const sliders = document.querySelectorAll('.div-slider');
+
+sliders.forEach(function(sliderDiv){
+    const slidesWrapper = sliderDiv.querySelector('.slides-wrapper')
+    const slides = slidesWrapper.querySelectorAll('.slider')
+    const container = sliderDiv.closest('section');
+    const menuSlider = container.querySelector('.menu-slider')
+    const seletores = container.querySelectorAll('.seletor-slider');
+    const botaoEsquerda = container.querySelector('.seta-esquerda-slider');
+    const botaoDireita = container.querySelector('.seta-direita-slider');
+    const botaoInicio = container.querySelector('.botao-slider-inicio')
+    const totalSlides = slides.length
+    let sliderAtual = 0
+
+    function atualizarSeletores(){
+        seletores.forEach((seletor, idx) =>{
+            if (idx === sliderAtual){
+                seletor.classList.add('seletor-slider-ativo')
+            } else {
+                seletor.classList.remove("seletor-slider-ativo")
+            }
+        })
+    }
+
+    function atualizarSlider(){
+        const deslocamento = -(100 / totalSlides) * sliderAtual;
+        sliderDiv.style.transform = `translateX(${deslocamento}%)`;
+        atualizarSeletores()
+
+        if (menuSlider) {
+            if (sliderAtual > 0) {
+                menuSlider.classList.add('menu-slider-visivel')
+            } else {
+                menuSlider.classList.remove('menu-slider-visivel')
+            }
+        }
+    }
+
+    if (botaoDireita) {
+        botaoDireita.addEventListener('click', () => {
+            if (sliderAtual < totalSlides - 1){
+                sliderAtual++;
+                atualizarSlider()
+            }
+        })
+    }
+
+    if (botaoEsquerda){
+        botaoEsquerda.addEventListener('click', () => {
+            if (sliderAtual > 0){
+                sliderAtual--;
+                atualizarSlider()
+            }
+        })
+    }
+
+    seletores.forEach((seletor, idx) => {
+        seletor.addEventListener('click', () => {
+            sliderAtual = idx;
+            atualizarSlider();
+
+        })
+    })
+
+    if (botaoInicio){
+        botaoInicio.addEventListener('click', () => {
+            sliderAtual++
+            atualizarSlider()
+        })
+    }
+
+    atualizarSeletores()
+})
